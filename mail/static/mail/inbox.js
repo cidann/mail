027c1d76bb7b436a797cd4437b0dbf447143d234
Rui Chen <chenruix47@gmail.com>
2022-05-24 00:30:15 -0400

    added reply

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -54,12 +54,14 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  
+  //display each email
   fetch(`emails/${mailbox}`)
   .then(response=>response.json())
   .then(data=>{
     data.forEach(email=>{
       //container for email
-      div=document.createElement('div');
+      let div=document.createElement('div');
       div.className='email';
       div.dataset.id=email['id']
       //container for sender of email
@@ -84,49 +86,78 @@ function load_mailbox(mailbox) {
         div.style.backgroundColor='lightgray';
       }
       //detail of email on click
-      div.onclick=()=>{details(div)}
+      div.onclick=()=>{details(div,mailbox)}
     });
   });
-  function details(email){
-    document.querySelector('#emails-view').style.display = 'none';
-    document.querySelector('#email-detail').style.display = 'block';
-    document.querySelector('#compose-view').style.display = 'none';
+  
+}
+function details(email,mailbox){
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#email-detail').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#email-detail').innerHTML='';
 
-    document.querySelector('#email-detail').innerHTML='';
-    
-    //get details
-    fetch(`emails/${email.dataset.id}`)
-    .then(response=>response.json())
-    .then(data=>{
-      div=document.querySelector('#email-detail');
-      //create container with sender
-      sender=document.createElement('div');
-      sender.innerHTML=`From: ${data['sender']}`;
-      div.append(sender);
-      //create container with recipients
-      recipients=document.createElement('div');
-      recipients.innerHTML=`To: ${data['recipients']}`;
-      div.append(recipients);
-      //create container with subject
-      subject=document.createElement('div');
-      subject.innerHTML=`Subject: ${data['subject']}`;
-      div.append(subject);
-      //create container with timestamp
-      timestamp=document.createElement('div');
-      timestamp.innerHTML=`Timestamp: ${data['timestamp']}`;
-      div.append(timestamp);
-      //Seperation line
-      div.append(document.createElement('hr'))
-      //create container with body
-      body=document.createElement('div');
-      body.innerHTML=data['body'];
-      div.append(body);
-    })
-    //mark email as read
-    fetch(`emails/${email.dataset.id}`,{
-      method:"PUT",
-      body:JSON.stringify({read:true})
-    })
-  }
+  //get details
+  fetch(`emails/${email.dataset.id}`)
+  .then(response=>response.json())
+  .then(data=>{
+    div=document.querySelector('#email-detail');
+    //create container with sender
+    sender=document.createElement('div');
+    sender.innerHTML=`From: ${data['sender']}`;
+    sender.id='sender';
+    div.append(sender);
+    //create container with recipients
+    recipients=document.createElement('div');
+    recipients.innerHTML=`To: ${data['recipients']}`;
+    div.append(recipients);
+    //create container with subject
+    subject=document.createElement('div');
+    subject.innerHTML=`Subject: ${data['subject']}`;
+    div.append(subject);
+    //create container with timestamp
+    timestamp=document.createElement('div');
+    timestamp.innerHTML=`Timestamp: ${data['timestamp']}`;
+    div.append(timestamp);
+
+    //reply and archive button depending on mailbox the email is in
+    if(mailbox=='inbox'){
+      reply=document.createElement('button');
+      archive=document.createElement('button');
+      div.append(reply);
+      div.append(archive);
+    }
+    //Seperation line
+    div.append(document.createElement('hr'))
+    //create container with body
+    body=document.createElement('div');
+    body.innerHTML=data['body'];
+    div.append(body);
+
+    //defining the button added
+    reply.className="btn btn-sm btn-outline-primary";
+    reply.innerHTML="Reply";
+    reply.onclick=()=>{
+      compose_email();
+      document.querySelector('#compose-recipients').value = sender.innerHTML.replace('From: ','');
+      if(subject.innerHTML.indexOf('Re: ')===-1)
+        document.querySelector('#compose-subject').value = `Re: ${subject.innerHTML.replace('Subject: ','')}`;
+      else
+      document.querySelector('#compose-subject').value = subject.innerHTML;
+      
+      document.querySelector('#compose-body').value = `On ${timestamp.innerHTML.replace('Timestamp: ','')} ${sender.innerHTML.replace('From: ','')} wrote:\n${body.innerHTML}`;
+    }
+    archive.className="btn btn-sm btn-outline-primary";
+    archive.innerHTML="Archive";
+  
+  })
+  markRead(email);
 }
 
+function markRead(email){
+  //mark email as read
+  fetch(`emails/${email.dataset.id}`,{
+    method:"PUT",
+    body:JSON.stringify({read:true})
+  })
+}
